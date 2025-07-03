4b7437c6968c07d4f574c40cd8d3842e0b3c7f76
Michel Villanueva <mhernande1@bnl.gov>
2025-07-03 15:29:40 -0400

    README for current DB schema

--- init-db/01-create-extensions.sql
+++
@@ -1,5 +0,0 @@
--- Enable required PostgreSQL extensions for the fast monitoring database
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
--- Grant necessary permissions to the postgres user
-GRANT ALL PRIVILEGES ON DATABASE swf_fastmonitoring TO postgres;
